LegacyXperiaAOSP/android_art

ART: Add scorpion to the known cpu variants list
* Avoid logcat spam of:
10-26 20:37:57.846  2153  2153 W dex2oat : Unknown instruction set features for
ARM CPU variant (scorpion) using conservative defaults

Change-Id: I07db465e1f4a9c4cfb69b8d271ed41ec9a16eee5

runtime/arch/arm/instruction_set_features_arm.cc
@@ -88,7 +88,7 @@ const ArmInstructionSetFeatures* ArmInstructionSetFeatures::FromVariant(
         "arm1136j-s", "arm1136jf-s",
         "arm1156t2-s", "arm1156t2f-s", "arm1176jz-s", "arm1176jzf-s",
         "cortex-a5", "cortex-a8", "cortex-a9", "cortex-a9-mp", "cortex-r4f",
-        "marvell-pj4", "mpcore", "mpcorenovfp"
+        "marvell-pj4", "mpcore", "mpcorenovfp", "scrorpion"
     };
     if (!FindVariantInArray(arm_variants_without_known_features,
                             arraysize(arm_variants_without_known_features),

halogenOS/-project

Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

runtime/arch/arm/instruction_set_features_arm.cc
@@ -88,7 +88,7 @@ const ArmInstructionSetFeatures* ArmInstructionSetFeatures::FromVariant(
         "arm1136j-s", "arm1136jf-s",
         "arm1156t2-s", "arm1156t2f-s", "arm1176jz-s", "arm1176jzf-s",
         "cortex-a5", "cortex-a8", "cortex-a9", "cortex-a9-mp", "cortex-r4f",
-        "marvell-pj4", "mpcore", "mpcorenovfp"
+        "marvell-pj4", "mpcore", "mpcorenovfp", "scorpion"
     };
     if (!FindVariantInArray(arm_variants_without_known_features,
                             arraysize(arm_variants_without_known_features),